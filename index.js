6d5de475a609c0394b1b3ed697c8f0754c9890a9
Jhon Cabeza <cabejhon7@gmail.com>
2023-04-19 17:08:44 -0500

    Finish Count

--- index.js
+++ index.js
@@ -6,13 +6,11 @@ const second = 1000,
 let countDown = new Date('April 19, 2023 16:50:00').getTime(),
     x = setInterval(function () {
 
-        let now = new Date().getTime(),
-            distance = countDown - now;
 
-        document.getElementById('dias').innerText = Math.floor(distance / (day)),
-            document.getElementById('horas').innerText = Math.floor((distance % (day)) / (hour)),
-            document.getElementById('minutos').innerText = Math.floor((distance % (hour)) / (minute)),
-            document.getElementById('segundos').innerText = Math.floor((distance % (minute)) / second);
+            document.getElementById('dias').innerText = 0,
+            document.getElementById('horas').innerText = 0,
+            document.getElementById('minutos').innerText = 0,
+            document.getElementById('segundos').innerText = 0;
 
     }, second)
 
