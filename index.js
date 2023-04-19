ecada18758258c7e524f1a75018cfcf5458b2748
Jhon Cabeza <cabejhon7@gmail.com>
2023-04-19 14:41:53 -0500

    Add 1 hour

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const second = 1000,
     hour = minute * 60,
     day = hour * 24;
 
-let countDown = new Date('April 19, 2023 15:00:00').getTime(),
+let countDown = new Date('April 19, 2023 16:00:00').getTime(),
     x = setInterval(function () {
 
         let now = new Date().getTime(),
